4023982591d6dd51d87404ad2fe22252f9ee4fdd
fernoe1 <krutoytemirlan2007@mail.ru>
2025-03-27 20:49:20 +0600

    Bonus Task - String Compression

---
+++ src/problems/bonus/StringCompression.java
@@ -0,0 +1,47 @@
+package problems.bonus;
+
+import java.util.Scanner;
+
+public class StringCompression {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // Scanner to read user input
+        String s = sc.next(); // Base parameter s
+
+        double startTime = System.nanoTime();
+        System.out.println(compress(s));
+        double endTime = System.nanoTime();
+        System.out.println("Time taken: " + ((endTime - startTime) / 1000000) + " milliseconds");
+    }
+
+    /**
+     * This function initializes main function with right parameters. <br>
+     * Runs in O(n) complexity.
+     * @param s String to compress.
+     * @return Compressed string.
+     */
+    public static String compress(String s) {
+        return s.charAt(0) + compress(s, s.charAt(0), 1, 0);
+    }
+
+    /**
+     * Compresses string, if concisely aaaa becomes a3. <br>
+     * Runs in O(n) complexity.
+     * @param s String to compress.
+     * @param currentChar Current character.
+     * @param index Current index.
+     * @param counter Counts consecutive characters.
+     * @return Compressed string.
+     */
+    public static String compress(String s, char currentChar, int index, int counter) {
+        if (index == s.length()) {
+            return counter > 1 ? "" + counter : "";
+        }
+
+        if (s.charAt(index) == currentChar) {
+            return compress(s, currentChar, index + 1, counter + 1);
+        } else {
+            String result = counter > 1 ? "" + counter + s.charAt(index): "" + s.charAt(index);
+            return result + compress(s, s.charAt(index), index + 1, 1);
+        }
+    }
+}
